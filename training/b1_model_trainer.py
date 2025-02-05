3b566ae88e0de78293f286100afea084dc5dd8d7
Luai Waleed <mistaluai@gmail.com>
2025-02-05 06:05:37 +0200

    major resnetevo refactoring
    for more extensibility and readability i separated the training into another trainer class, to be able to handle more complex tasks like multi gpu/tpu training in the future, a decoupling was much needed, as the trainer class will handle the parallel data dist. in the future versions

---
+++ training/b1_model_trainer.py
@@ -0,0 +1,79 @@
+from tqdm import tqdm
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch_xla
+import torch_xla.core.xla_model as xm
+import torchvision
+from tqdm import tqdm
+
+class b1_ModelTrainer:
+    def __init__(self, model, optimizer, criterion, epochs, dataloaders, device, is_tpu=False):
+        self.model = model
+        self.optimizer = optimizer
+        self.criterion = criterion
+        self.epochs = epochs
+        self.dataloaders = dataloaders
+        self.DEVICE = device
+
+    def train_model(self):
+        model, optimizer, criterion, epochs, dataloaders = self.model, self.optimizer, self.criterion, self.epochs, self.dataloaders
+        for epoch in range(epochs):
+            ## change model mode depending on the phase
+            for phase in ['train', 'val']:
+                dataloader = dataloaders[phase]
+                epoch_loss = 0  # Track total loss for the epoch
+                if phase == 'train':
+                    model.train()
+                    for inputs, labels in tqdm(dataloader, desc=phase):
+                        inputs, labels = inputs.to(self.DEVICE), labels.to(self.DEVICE)
+
+                        # zero grads of he optim
+                        optimizer.zero_grad()
+                        # freeze the non-learnable weights
+                        self.__handle_transfer_learning(phase, epoch // epochs)
+                        # forward pass
+                        logit = model(inputs)
+                        loss = criterion(logit, labels)
+                        loss.backward()
+                        # update weights
+                        self.__optimizer_step(optimizer)
+                        epoch_loss += loss.item()  # Accumulate loss
+
+                else:
+                    # skip evaluation if no suitable dataloader
+                    if dataloaders[phase] is None:
+                        continue
+                    model.eval()
+                    self.__eval_model(dataloader)
+
+                print(f"Epoch {epoch + 1}/{epochs}, {phase} Loss: {epoch_loss / len(dataloader)}")  # Print loss
+
+
+    def __optimizer_step(self, optimizer):
+        if self.is_tpu:
+            xm.optimizer_step(optimizer)
+        else:
+            optimizer.step()
+
+
+    def __handle_transfer_learning(self, phase, ratio_epochs, tl_coeff=0.8):
+        if phase == "train":
+            if ratio_epochs >= tl_coeff:
+                # Unfreeze all layers for fine-tuning
+                for param in self.model.parameters():
+                    param.requires_grad = True
+            else:
+                # Freeze the CNN part
+                for param in self.model.parameters():
+                    param.requires_grad = False
+                # Unfreeze the classification layer
+                for param in self.model.fc.parameters():
+                    param.requires_grad = True
+        elif phase == "val":
+            for param in self.model.parameters():
+                param.requires_grad = False
+
+
+    def __eval_model(self, dataloader):
+        raise NotImplementedError('Not implemented yet')
