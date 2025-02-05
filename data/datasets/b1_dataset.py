aae476e3812f7b1a8f45fac653fdc4b3d4a4723f
Luai Waleed <mistaluai@gmail.com>
2025-02-05 06:22:45 +0200

    Updated the b1 dataset to be able to specify which split you want (train or test or val)

--- data/datasets/b1_dataset.py
+++ data/datasets/b1_dataset.py
@@ -9,7 +9,14 @@ from torchvision.transforms import v2
 
 
 class B1Dataset(Dataset):
-    def __init__(self, csv_file, transform=None):
+
+    VIDEO_SPLITS = {
+        'train': {1, 3, 6, 7, 10, 13, 15, 16, 18, 22, 23, 31, 32, 36, 38, 39, 40, 41, 42, 48, 50, 52, 53, 54},
+        'val': {0, 2, 8, 12, 17, 19, 24, 26, 27, 28, 30, 33, 46, 49, 51},
+        'test': {4, 5, 9, 11, 14, 20, 21, 25, 29, 34, 35, 37, 43, 44, 45, 47}
+    }
+
+    def __init__(self, csv_file, split='train', transform=None):
         self.data = pd.read_csv(csv_file)
         if transform is None:
             self.transform = v2.Compose([
@@ -17,6 +24,11 @@ class B1Dataset(Dataset):
             ])
         else:
             self.transform = transform
+
+        if split in self.VIDEO_SPLITS:
+            self.data = self.data[self.data['video_names'].astype(int).isin(self.VIDEO_SPLITS[split])]
+        else:
+            raise NameError(f'There is no such split: {split}, only {self.VIDEO_SPLITS}')
 
     def __len__(self):
         return len(self.data)
