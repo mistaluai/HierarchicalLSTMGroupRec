4f980b8c2bdeb6a18013d9ab5bad7373e3b0e403
Luai Waleed <mistaluai@gmail.com>
2025-02-06 07:09:16 +0200

    Fixing issues in training loop

--- training/b1_model_trainer_gpu.py
+++ training/b1_model_trainer_gpu.py
@@ -14,11 +14,11 @@ class b1_ModelTrainer:
         model, optimizer, criterion, epochs, dataloaders = self.model, self.optimizer, self.criterion, self.epochs, self.dataloaders
 
         epoch = 0
-        loss = 0
         if self.is_continue:
-            epoch, model, optimizer, loss = self.__load_checkpoint(model, optimizer, self.checkpoint)
+            epoch, model, optimizer = self.__load_checkpoint(model, optimizer, self.checkpoint)
 
         for training_epoch in range(epoch, epochs):
+            print(f"Training epoch {training_epoch}: training {'full model' if self.__check_transfer_learning(training_epoch/epochs) else 'only fc model'}")
             ## change model mode depending on the phase
             for phase in ['train', 'val']:
                 dataloader = dataloaders[phase]
@@ -39,20 +39,24 @@ class b1_ModelTrainer:
                         # update weights
                         optimizer.step()
                         epoch_loss += loss.item()  # Accumulate loss
+                    print(
+                        f"Epoch {training_epoch + 1}/{epochs}, {phase} Loss: {epoch_loss / len(dataloader)}")  # Print loss
                 else:
                     # skip evaluation if no suitable dataloader
                     if dataloaders[phase] is None:
                         continue
                     model.eval()
-                    self.__eval_model(dataloader)
-            self.__save_checkpoint(training_epoch, model.state_dict(), optimizer.state_dict(), epoch_loss)
-            print(f"Epoch {training_epoch + 1}/{epochs}, {phase} Loss: {epoch_loss / len(dataloader)}")  # Print loss
+                    loss, acc = self.__eval_model(dataloader)
+                    print(f"Epoch {training_epoch + 1}/{epochs}, ({phase}) Loss: {loss} | Accuracy: {acc}")  # Print loss
+
+
+            self.__save_checkpoint(training_epoch, model.state_dict(), optimizer.state_dict())
 
         self.__save_model()
 
     def __handle_transfer_learning(self, phase, ratio_epochs, tl_coeff=0.8):
         if phase == "train":
-            if ratio_epochs >= tl_coeff:
+            if self.__check_transfer_learning(ratio_epochs, tl_coeff):
                 # Unfreeze all layers for fine-tuning
                 for param in self.model.parameters():
                     param.requires_grad = True
@@ -66,6 +70,10 @@ class b1_ModelTrainer:
         elif phase == "val":
             for param in self.model.parameters():
                 param.requires_grad = False
+
+    def __check_transfer_learning(self, ratio_epochs, tl_coeff=0.8):
+        return ratio_epochs >= tl_coeff
+
 
     def __eval_model(self, dataloader):
         model = self.model
@@ -97,21 +105,19 @@ class b1_ModelTrainer:
     def __save_model(self):
         torch.save(self.model.state_dict(), self.save_folder + "/b1_model.pth")
 
-    def __save_checkpoint(self, epoch, model_state_dict, optimizer_state_dict, loss):
+    def __save_checkpoint(self, epoch, model_state_dict, optimizer_state_dict):
         checkpoint = {
             'epoch': epoch,
             'model_state_dict': model_state_dict,
             'optimizer_state_dict': optimizer_state_dict,
-            'loss': loss,
         }
-        torch.save(checkpoint, self.save_folder + f'checkpoint-epoch{epoch}-loss{loss}.pth')
+        torch.save(checkpoint, self.save_folder + f'/checkpoint-epoch{epoch}.pth')
 
     def __load_checkpoint(self, model, optimizer, checkpoint_path):
         checkpoint = torch.load(checkpoint_path)
         epoch = checkpoint['epoch']
         model_state_dict = checkpoint['model_state_dict']
         optimizer_state_dict = checkpoint['optimizer_state_dict']
-        loss = checkpoint['loss']
         model = model.load_state_dict(model_state_dict)
         optimizer = optimizer.load_state_dict(optimizer_state_dict)
-        return epoch, model, optimizer, loss
+        return epoch, model, optimizer
