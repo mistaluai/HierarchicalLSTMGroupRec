0b75ae995bbda0012264ced625b97dfbfad5c9aa
Luai Waleed <mistaluai@gmail.com>
2025-02-06 15:10:27 +0200

    defined custom weighted loss for the dataset class imbalance problem

---
+++ loss_fns/b1_weighted_cross_entropy.py
@@ -0,0 +1,23 @@
+import torch
+from collections import Counter
+
+class WeightedCrossEntropyLoss(torch.nn.Module):
+    def __init__(self, dataset, device):
+        super(WeightedCrossEntropyLoss, self).__init__()
+        self.dataset = dataset
+        self.device = device
+        weight = self.__compute_weights()
+        self.loss = nn.CrossEntropyLoss(weight=weight)
+
+    def __compute_weights(self):
+        label_counts = Counter([label.item() for _, label in self.dataset])
+        total_samples = len(self.dataset)
+
+        class_weights = [total_samples / label_counts[i] if i in label_counts else 0 for i in range(8)]
+
+        weights = torch.tensor(class_weights, dtype=torch.float32).to(self.device)
+
+        return weights
+
+    def forward(self, logit, target):
+        return self.loss(logit, target)
