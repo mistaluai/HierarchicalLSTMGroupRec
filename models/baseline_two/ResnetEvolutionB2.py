242d309c92d2005ccffea62e58a619969120f7ac
Luai Waleed <mistaluai@gmail.com>
2025-02-09 03:58:26 +0200

    removed redundant training code from b2 model

--- models/baseline_two/ResnetEvolutionB2.py
+++ models/baseline_two/ResnetEvolutionB2.py
@@ -49,66 +49,6 @@ class ResnetEvolution(nn.Module):
 
         self.classifer = nn.Sequential(*layers)  # Output layer for binary classification
 
-
-    def train_model(self, criterion, epochs, dataloaders):
-        model = self.model
-        optimizer = self.optimizer
-        for epoch in range(epochs):
-            ## change model mode depending on the phase
-            for phase in ['train', 'val']:
-                dataloader = dataloaders[phase]
-                epoch_loss = 0  # Track total loss for the epoch
-                if phase == 'train':
-                    model.train()
-                    for inputs, labels in tqdm(dataloader, desc=phase):
-                        inputs, labels = inputs.to(self.DEVICE), labels.to(self.DEVICE)
-
-                        # zero grads of he optim
-                        optimizer.zero_grad()
-                        # freeze the non-learnable weights
-                        self.__handle_transfer_learning(phase, epoch // epochs)
-                        # forward pass
-                        logit = model(inputs)
-                        loss = criterion(logit, labels)
-                        loss.backward()
-                        # update weights
-                        self.__optimizer_step(optimizer)
-                        epoch_loss += loss.item()  # Accumulate loss
-
-                else:
-                # skip evaluation if no suitable dataloader
-                    if dataloaders[phase] is None:
-                        continue
-                    model.eval()
-                    self.__eval_model(dataloader)
-
-                print(f"Epoch {epoch + 1}/{epochs}, {phase} Loss: {epoch_loss / len(dataloader)}")  # Print loss
-
-    def __optimizer_step(self, optimizer):
-        if self.is_tpu:
-            xm.optimizer_step(optimizer)
-        else:
-            optimizer.step()
-
-    def __handle_transfer_learning(self, phase, ratio_epochs, tl_coeff=0.8):
-        if phase == "train":
-            if ratio_epochs >= tl_coeff:
-                # Unfreeze all layers for fine-tuning
-                for param in self.model.parameters():
-                    param.requires_grad = True
-            else:
-                # Freeze the CNN part
-                for param in self.model.parameters():
-                    param.requires_grad = False
-                # Unfreeze the classification layer
-                for param in self.model.fc.parameters():
-                    param.requires_grad = True
-        elif phase == "val":
-            for param in self.model.parameters():
-                param.requires_grad = False
-                
-    def __eval_model(self, dataloader):
-        raise NotImplementedError('Not implemented yet')
     
     def forward(self, images):
 
