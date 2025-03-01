71d4cdea095960a91f19d8daa3b06bd45e4fef01
Luai Waleed <mistaluai@gmail.com>
2025-03-01 23:45:25 +0200

    implemented more efficient and simple annotations processor for b1

---
+++ data/datasets/b1_dataprocessor.py
@@ -0,0 +1,76 @@
+import os
+
+from charset_normalizer.md import annotations
+
+
+class DataProcessorBaselineOne():
+    def __init__(self, videos_root):
+        self.root = videos_root
+        self.data_classes = self.concat_annotations()
+        self.dataset = self.collect_video_data()
+
+    def get_dataset(self):
+        return self.dataset
+
+    def __process_annotations(self, annotations_file):
+        data_dict = {}
+
+        with open(annotations_file, 'r') as file:
+            for line in file:
+                parts = line.strip().split()
+
+                if len(parts) > 1:  # Ensure there are at least 2 words
+                    filename = parts[0]  # First word is the filename
+                    action = parts[1]  # Second word is the action
+                    data_dict[filename] = action
+
+        return data_dict
+
+    def concat_annotations(self):
+        data_classes = {}
+        for video_folder in sorted(os.listdir(self.root)):  # Iterate over videos
+            video_path = os.path.join(self.root, video_folder)
+
+            if not os.path.isdir(video_path):
+                continue  # Skip files, process only directories
+
+            for annotation_folder in sorted(os.listdir(video_path)):  # Iterate over clips
+                annotation_path = os.path.join(video_path, annotation_folder)
+                if not os.path.isdir(annotation_path):
+                    if annotation_folder == 'annotations.txt':
+                        data_classes = {**self.__process_annotations(annotation_path), **data_classes}
+
+        return data_classes
+
+    def collect_video_data(self):
+        dataset = []  # List to store video data
+        missing = 0
+        for video_folder in sorted(os.listdir(self.root)):  # Iterate over videos
+            video_path = os.path.join(self.root, video_folder)
+            if not os.path.isdir(video_path):
+                continue  # Skip files, process only directories
+
+            for clip_folder in sorted(os.listdir(video_path)):  # Iterate over clips
+                clip_path = os.path.join(video_path, clip_folder)
+                if not os.path.isdir(clip_path):
+                    continue
+
+                frames = sorted(
+                    [os.path.join(clip_path, f) for f in os.listdir(clip_path) if f.endswith(".jpg")]
+                )
+                if len(frames) == 41:  # Ensure expected number of frames
+                    target_frame = frames[20]
+                    frame_name = target_frame.split('/')[-1]
+                    if frame_name in self.data_classes:
+                        class_name = self.data_classes[frame_name]
+                        dataset.append({
+                            "video": video_folder,
+                            "clip": clip_folder,
+                            "frame": target_frame,
+                            "class": class_name
+                        })
+                    else:
+                        missing += 1
+
+        print(f'missing {missing} frames')
+        return dataset
