07d006501158cf2e064b0285ddbc56010e6c3e2d
Luai Waleed <mistaluai@gmail.com>
2025-03-05 07:34:45 +0200

    updated b2 model

--- models/baseline_two/ResnetEvolution.py
+++ models/baseline_two/ResnetEvolution.py
@@ -1,33 +1,22 @@
-import torch
-import torch.nn as nn
-import torch.optim as optim
-import torchvision
-from torchvision.models import ResNet50_Weights
-from tqdm import tqdm
-
-
 class ResnetEvolution(nn.Module):
     def __init__(self):
         super(ResnetEvolution, self).__init__()
-        self.model, backbone_outputs = self.__init_backbone(torchvision.models.resnet50(weights=ResNet50_Weights.IMAGENET1K_V2))
+        self.model = self.__init_backbone(torchvision.models.resnet50(weights=ResNet50_Weights.IMAGENET1K_V2, progress=False))
 
-        self.layers = nn.Sequential(
-            nn.Linear(backbone_outputs, 4096),
+    def __init_backbone(self, backbone):
+        num_features = backbone.fc.in_features
+        layers = nn.Sequential(
+            nn.Linear(num_features, 4096),
             nn.Linear(4096, 2048),
             nn.ReLU(),
             nn.Dropout(0.2),
             nn.Linear(2048, 9)
         )
-
-    def __init_backbone(self, backbone):
-        num_features = backbone.fc.in_features
-        backbone.fc = None
-        return backbone, num_features
+        backbone.fc = layers
+        return backbone
 
     def get_fc(self):
-        return self.layers
+        return self.model.fc
 
     def forward(self, images):
-        features = self.model(images)
-        logits = self.layers(features)
-        return logits
+        return self.model(images)
