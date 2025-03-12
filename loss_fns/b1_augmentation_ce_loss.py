178cdae1a0129672ce328bc175e7e2f21eb111f4
Luai Waleed <mistaluai@gmail.com>
2025-03-12 02:23:09 +0200

    fixed inheritance

--- loss_fns/b1_augmentation_ce_loss.py
+++ loss_fns/b1_augmentation_ce_loss.py
@@ -3,7 +3,7 @@ import torch.nn.functional as F
 
 class AugmentationCELoss(WeightedCrossEntropyLoss):
     def __init__(self, dataset, device, num_classes):
-        super(AugmentationCELoss, self).__init__(dataset, device, num_classes)
+        super().__init__(dataset, device, num_classes)
 
     def forward(self, logit, target):
         return F.cross_entropy(logit, target, weight=self.weight)
