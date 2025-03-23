70fdaf9b442b94b1f1b17510586d53830d110b56
Luai Waleed <mistaluai@gmail.com>
2025-03-23 07:18:07 +0200

    fixing issue in model baseline 3

--- models/baseline_three/ResnetEvolution.py
+++ models/baseline_three/ResnetEvolution.py
@@ -8,16 +8,16 @@ class ResnetEvolution(nn.Module):
             feature_extractor (nn.Module): Pre-trained ResNet model for feature extraction.
         """
         super(ResnetEvolution, self).__init__()
-        
-        
+
+
         self.feature_extractor = self.__load_resnet_feature_extractor(feature_extractor_path)
 
         self.group_fc = nn.Sequential(
             nn.Linear(2048, 512),
             nn.ReLU(),
-            nn.Linear(512, 9)  
+            nn.Linear(512, 9)
         )
-        
+
 
     def forward(self, players):
         """
@@ -27,22 +27,22 @@ class ResnetEvolution(nn.Module):
             group_activity_logits: torch.Size([batch_size, 9])
         """
         batch_size, num_players, C, H, W = players.shape
-        players = players.view(batch_size * num_players, C, H, W)  
-        with torch.no_grad(): 
-            player_features = self.feature_extractor(players) 
-        player_features = player_features.view(batch_size, num_players, -1)  
-        pooled_features = torch.max(player_features, dim=1)[0] 
-        group_activity_logits = self.group_fc(pooled_features) 
+        players = players.view(batch_size * num_players, C, H, W)
+        with torch.no_grad():
+            player_features = self.feature_extractor(players)
+        player_features = player_features.view(batch_size, num_players, -1)
+        pooled_features = torch.max(player_features, dim=1)[0]
+        group_activity_logits = self.group_fc(pooled_features)
 
         return group_activity_logits
 
 
-    def __load_resnet_feature_extractor(state_dict_path):
-        
+    def __load_resnet_feature_extractor(self, state_dict_path):
+
         state_dict = torch.load(state_dict_path)
         model = FinetunableResnet()
         model.load_state_dict(state_dict, strict=False)  # strict=False in case extra keys exist
-        
+
         feature_extractor = nn.Sequential(*(list(model.children())[:-1]))
-        
+
         return feature_extractor
