0bdcd971b60d0a10241198f3439ed4f4a238af09
ahmedxaldeeb <ahmedaldiep@gmail.com>
2025-03-23 06:10:01 +0200

    Baseline3 model

---
+++ models/baseline_three/ResnetEvolution.py
@@ -0,0 +1,35 @@
+import torch
+import torch.nn as nn
+
+class ResnetEvolution(nn.Module):
+    def __init__(self, feature_extractor):
+        """
+        Args:
+            feature_extractor (nn.Module): Pre-trained ResNet model for feature extraction.
+        """
+        super(ResnetEvolution, self).__init__()
+        
+        self.feature_extractor = feature_extractor
+
+        self.group_fc = nn.Sequential(
+            nn.Linear(2048, 512),
+            nn.ReLU(),
+            nn.Linear(512, 9)  
+        )
+
+    def forward(self, players):
+        """
+        Args:
+            players: torch.Size([batch_size, num_players, 3, 224, 224])
+        Returns:
+            group_activity_logits: torch.Size([batch_size, 9])
+        """
+        batch_size, num_players, C, H, W = players.shape
+        players = players.view(batch_size * num_players, C, H, W)  
+        with torch.no_grad(): 
+            player_features = self.feature_extractor(players) 
+        player_features = player_features.view(batch_size, num_players, -1)  
+        pooled_features = torch.max(player_features, dim=1)[0] 
+        group_activity_logits = self.group_fc(pooled_features) 
+
+        return group_activity_logits
