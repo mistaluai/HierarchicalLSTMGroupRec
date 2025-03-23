36170e6526d271eaed84f1bc441e584d7b43eea9
ahmedxaldeeb <ahmedaldiep@gmail.com>
2025-03-23 06:23:55 +0200

    baseline3 dataloader

---
+++ models/baseline_three/Trainer.py
@@ -0,0 +1,170 @@
+class b1_ModelTrainer:
+    def __init__(self, model, optimizer, scheduled, criterion, epochs, dataloaders, device, save_folder,
+                 is_continue=False, checkpoint=None):
+        self.model = model
+        self.optimizer = optimizer
+        self.scheduled = scheduled
+        self.criterion = criterion
+        self.epochs = epochs
+        self.dataloaders = dataloaders
+        self.DEVICE = device
+        self.save_folder = save_folder
+        self.is_continue = is_continue
+        self.checkpoint = checkpoint
+        self.scaler = torch.amp.GradScaler()
+
+    # verbose 1 : checkpoint,
+    # verbose 3:  labels, preds
+    # verbose 4: logits
+    def train_model(self, verbose=0):
+        model, optimizer, criterion, epochs, dataloaders, scaler = self.model, self.optimizer, self.criterion, self.epochs, self.dataloaders, self.scaler
+
+        epoch = 0
+
+        train_losses = []
+        val_losses = []
+        val_accuracies = []
+
+        if self.is_continue:
+
+            if verbose > 0:
+                print(f"Continuing from checkpoint {self.checkpoint}")
+
+            epoch, model, optimizer = self.__load_checkpoint(model, optimizer, self.checkpoint, verbose)
+        
+        for training_epoch in range(epoch, epochs):
+
+            print(f"\nTraining epoch {training_epoch+1}")
+
+            ## change model mode depending on the phase
+            for phase in ['train', 'val']:
+                dataloader = dataloaders[phase]
+                epoch_loss = 0  # Track total loss for the epoch
+                if phase == 'train':
+                    if verbose > 0:
+                        dataloader = tqdm(dataloader, desc=phase)
+                    
+                    model.train()
+                    
+                    # frame, frame_class, player_images, player_labels
+                    for _, inputs, labels, _ in dataloader:
+
+                        inputs = inputs.to(self.DEVICE)
+                        labels = labels.to(self.DEVICE)
+
+                        if verbose > 3:
+                            print(f"labels: {labels}")
+
+                        # zero grads of he optim
+                        optimizer.zero_grad()
+
+                        # freeze the non-learnable weights
+
+                        with torch.amp.autocast('cuda'):
+                            # forward pass
+                            logit = model(inputs)
+
+                            if verbose > 3:
+                                print(f"logit: {logit}")
+
+                            loss = criterion(logit, labels)
+
+                        scaler.scale(loss).backward()
+                        optimizer.step(scaler)
+                        scaler.update()
+
+                        epoch_loss += loss.item()  # Accumulate loss
+
+                    train_losses.append(epoch_loss / len(dataloader))
+                    print(
+                        f"Epoch {training_epoch + 1}/{epochs}, {phase} Loss: {epoch_loss / len(dataloader)}")  # Print loss
+                else:
+                    # skip evaluation if no suitable dataloader
+                    if dataloaders[phase] is None:
+                        continue
+                    model.eval()
+                    loss, acc = self.__eval_model(dataloader, verbose)
+                    val_losses.append(loss)
+                    val_accuracies.append(acc)
+                    print(
+                        f"Epoch {training_epoch + 1}/{epochs}, ({phase}) Loss: {loss} | Accuracy: {acc}")  # Print loss
+
+            if self.scheduled:
+                optimizer.scheduler_step()
+                TrainingUtilities.save_checkpoint(training_epoch, model.state_dict(), optimizer.optimizer_state_dict(),
+                                       optimizer.scheduler_state_dict(), self.save_folder, verbose)
+            else:
+                TrainingUtilities.save_checkpoint(training_epoch, model.state_dict(), optimizer.state_dict(),self.save_folder, verbose)
+
+            if training_epoch % 10 == 0:
+                TrainingUtilities.save_model(model, training_epoch, self.save_folder,verbose)
+
+        TrainingUtilities.save_model(model, 'final_', self.save_folder, verbose)
+        return train_losses, val_losses, val_accuracies
+
+    def __handle_transfer_learning(self, phase, ratio_epochs, tl_coeff=0, verbose=0):
+        if phase == "train":
+            if self.__check_transfer_learning(ratio_epochs, tl_coeff):
+                # Unfreeze all layers for fine-tuning
+                for param in self.model.parameters():
+                    param.requires_grad = True
+            else:
+                # Freeze the CNN part
+                for param in self.model.parameters():
+                    param.requires_grad = False
+                # Unfreeze the classification layer
+                for param in self.model.get_fc().parameters():
+                    param.requires_grad = True
+        elif phase == "val":
+            for param in self.model.parameters():
+                param.requires_grad = False
+
+    def __check_transfer_learning(self, ratio_epochs, tl_coeff=0):
+        return ratio_epochs >= tl_coeff
+
+    def __eval_model(self, dataloader, verbose=0):
+        model = self.model
+        criterion = self.criterion
+        model.eval()
+        val_loss = 0
+        correct_preds = 0
+        total_preds = 0
+        if verbose > 0:
+            dataloader = tqdm(dataloader, desc="Validation")
+        with torch.no_grad():
+            with torch.amp.autocast('cuda'):
+                for inputs, labels in dataloader:
+                    inputs = inputs.to(self.DEVICE)
+                    labels = labels.to(self.DEVICE)
+
+                    if verbose > 2:
+                        print(f"labels: {labels}")
+
+                    # Forward pass
+                    logits = model(inputs)
+
+                    if verbose > 3:
+                        print(f"logit: {logits}")
+
+                    probs = F.softmax(logits, dim=1)  # Apply softmax to get probabilities
+
+                    if verbose > 3:
+                        print(f"probs: {probs}")
+
+                    loss = criterion(logits, labels)
+                    val_loss += loss.item()  # Accumulate loss
+
+                    # Compute accuracy
+                    predicted = torch.argmax(probs, dim=1)  # Get the class with the highest probability
+
+                    if verbose > 2:
+                        print(f"predicted: {predicted}")
+                        print(f"true/false: {(predicted == labels)}")
+
+                    correct_preds += (predicted == labels).sum().item()
+                    total_preds += labels.size(0)
+
+        # Calculate average loss and accuracy
+        avg_loss = val_loss / len(dataloader)
+        accuracy = correct_preds / total_preds
+        return avg_loss, accuracy
