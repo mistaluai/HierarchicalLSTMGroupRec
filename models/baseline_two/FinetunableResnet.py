fc12050dd92dc9c76e7c2819d2e3854f6347a581
Luai Waleed <mistaluai@gmail.com>
2025-03-23 05:49:30 +0200

    the model used to classify player actions

--- models/baseline_two/FinetunableResnet.py
+++ models/baseline_two/FinetunableResnet.py
@@ -4,24 +4,15 @@ import torchvision.models as models
 class FinetunableResnet(nn.Module):
     def __init__(self):
         super(FinetunableResnet, self).__init__()
-        self.model = self.__get_model()
+        self.model = self.__init_backbone(torchvision.models.resnet50(weights=ResNet50_Weights.IMAGENET1K_V2, progress=False))
 
-    def __get_model(self):
-        resnet = models.resnet50(weights=None)
-        for name, param in resnet.named_parameters():
-            if name.startswith("conv1") or name.startswith("layer1") or name.startswith("layer2"):
-                param.requires_grad = False
-        num_features = resnet.fc.in_features
+    def __init_backbone(self, backbone):
+        num_features = backbone.fc.in_features
         layers = nn.Sequential(
-            nn.Linear(num_features, 4096),
-            nn.Linear(4096, 2048),
-            nn.ReLU(),
-            nn.Dropout(0.2),
-            nn.Linear(2048, 9)
+            nn.Linear(num_features, 9),
         )
-        resnet.fc = layers
-
-        return resnet
+        backbone.fc = layers
+        return backbone
 
     def get_fc(self):
         return self.model.fc
